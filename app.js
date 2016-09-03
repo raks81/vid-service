18f39c9bc16704ea88bf000c025728b6fc0a6dc9
Rakshith Rao (rakrao) <rakrao@cisco.com>
2016-09-03 16:17:00 +0530

    initial service

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
-var app = require('express');
+var express = require('express');
 var http = require('http');
-var qs = require('querystring');
-
-var API_KEY = 'AIzaSyDRl2Rc2rGksr8xHuKm3VbuXJIqrs6djtI';
+var fetch = require('./lib/fetch');
+var app = express();
 
 app.get('/playlist/:pid', function (req, res) {
-    var pid = req.query['pid'];
-    console.log('getting playlist items of %s', pid);
-    var query = {};
-    query.key = API_KEY;
-    query.part = 'snippet';
-    query.playlistId = pid;
-    var options = {
-        host: 'www.googleapis.com',
-        port: 443,
-        path: '/youtube/v3/playlistItems?' + qs.stringify(query),
-        method: 'GET'
-    };
-    console.log(JSON.stringify(options));
-    http.request(options, function (res) {
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log('BODY: ' + chunk);
-        });
-    }).end();
+    fetch.playlistItems(req.params.pid).then(function (pl) {
+        res.json(pl);
+    }, function (err) {
+        if (err) {
+            res.status(500).send({message: 'Could not fetch playlist: ' + req.params.pid})
+        }
+    });
 });
+
+var port = process.env.PORT || '8080';
+http.createServer(app).listen(port,
+    function () {
+        console.log("Express server listening on port %s", port);
+    }
+);
